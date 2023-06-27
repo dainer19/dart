95dff6b9a9ae005a4fdf0777c15667c0c2fdc35f
dainer alarcon <alarcondainer492@gmail.com>
2023-06-27 15:31:51 -0500

    actualizado

---
+++ ejercicios_dart/ejercicio2.dart
@@ -0,0 +1,6 @@
+void main() {
+  int num1 = 2;
+  int num2 = 2;
+  int suma = num1 + num2;
+  print('la suma de dos numeros es : $suma');
+}
---
+++ ejercicios_dart/ejercicio3.dart
@@ -0,0 +1,6 @@
+void main() {
+  double n1 = 3.4;
+  double n2 = 5.3;
+  double resta = n2 - n1;
+  print('la resta de dos numeros es: $resta');
+}

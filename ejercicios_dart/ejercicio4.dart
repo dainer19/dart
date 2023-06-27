7d464721a06f219b504941e0a136c21d353b3b94
dainer alarcon <alarcondainer492@gmail.com>
2023-06-27 17:19:38 -0500

    actualizado

---
+++ ejercicios_dart/ejercicio4.dart
@@ -0,0 +1,8 @@
+void main() {
+  /*determinar cuanto gana un aprendiz como monitor si recive el 10% de comidion*/
+  double subsidio = 1000000;
+  double comision = 0.10;
+  double porcentaje = subsidio * comision;
+  double ganancia = subsidio + porcentaje;
+  print('el aprendiz se gana :$ganancia');
+}
---
+++ ejercicios_dart/ejercicio5.dart
@@ -0,0 +1,12 @@
+void main() {
+  /*realizar un programa que lea tres numeros y determinar el cuadrado de cada uno de ellos*/
+  int n1 = 2;
+  int n2 = n1 * n1;
+  print('el cuadrado del $n1 numero es :$n2');
+  int n3 = 8;
+  int n4 = n3 * n3;
+  print('el cuadrado del $n3 numero es :$n4');
+  int n5 = 10;
+  int n6 = n5 * n5;
+  print('el cuadrado del $n5 numero es :$n6');
+}

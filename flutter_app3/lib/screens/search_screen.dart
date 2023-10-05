e818361171d8362305f7cb4a2a817ae99a1900dd
dainer <alarcondainer492@gmail.com>
2023-10-05 15:57:49 -0500

    dainer

--- flutter_app3/lib/screens/search_screen.dart
+++ flutter_app3/lib/screens/search_screen.dart
@@ -15,9 +15,21 @@ class Searchscreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("buscar"),
       ),
-      body: const Center(
-        child: Text("pantalla de busqueda"),
+      body:  Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget> [
+          const Text("buscador",style: TextStyle(fontSize: 24, color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
+         const SizedBox(height: 20,),
+          Image.asset("assets/images/descarga.jfif", width: 200,height: 200,),
+          const SizedBox(height: 20,),
+          const Text(
+            "¿Quieres buscar fotos a través de Internet? Esta herramienta buscar por imagen hace que sea muy rápido y fácil hacer una busqueda por imagen para uso personal o laboral. Simplemente sube una imagen o ingresa la URL de la imagen, o elige una imagen de Dropbox o de buscar imagen en google", textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, color: Colors.black),
+          )
+        ],),
       ),
      );
   }
 }
+
